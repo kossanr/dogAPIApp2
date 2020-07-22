39144f00d195403ed5ef3eacb38e00d8510ee8bf
kossanr <58185702+kossanr@users.noreply.github.com>
2020-07-22 12:26:18 -0600

    updates

---
+++ index.js
@@ -0,0 +1,27 @@
+function getImage(breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`) //user input inserted into URL
+    .then((res) => res.json())
+    .then((responseJson) => displayResults(responseJson))
+    .catch((error) => alert("Something went wrong"));
+}
+
+function displayResults(responseJson) {
+  $(".results-img").replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  );
+  //display the results section
+  $(".results").removeClass("hidden");
+}
+
+function watchForInput() {
+  //watchiong for submit event
+  $("form").submit((event) => {
+    event.preventDefault();
+    getImage($(".breed").val()); //runs getImage with the value of the user's input
+  });
+}
+
+$(function () {
+  console.log("App loaded, waiting for submit!");
+  watchForInput();
+});
